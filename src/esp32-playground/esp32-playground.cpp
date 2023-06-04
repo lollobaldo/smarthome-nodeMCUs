edbf51fcd5407a4a481e1ca84e2a3ca747a4e65b
Lorenzo Baldini <lollobaldo2000@gmail.com>
2023-06-04 15:47:41 +0100

    suppress

--- src/esp32-playground/esp32-playground.cpp
+++ src/esp32-playground/esp32-playground.cpp
@@ -18,49 +18,49 @@ const char* channelOutput = "playground/" CLIENT_NAME;
 
 std::vector<const char*> subscriptions{ channelCommands };
 
-const int sensorPin = A0;
-const int LED = 33;
-const long probingTime = 0.05*60*1000L; // 3 seconds
+// const int sensorPin = A0;
+// const int LED = 33;
+// const long probingTime = 0.05*60*1000L; // 3 seconds
 
-bool ledState = false;
+// bool ledState = false;
 
-Logger logger = Logger(channelLogs);
+// Logger logger = Logger(channelLogs);
 
-void callback(char* topic, byte* payload, unsigned int length) {
-    DebugPrint("Message arrived in topic ");
-    DebugPrint(topic);
-    DebugPrint(": ");
-    // add string terminator
-    char* message = (char*) payload;
-    message[length] = '\0';
-    DebugPrintln(message);
-}
+// void callback(char* topic, byte* payload, unsigned int length) {
+//     DebugPrint("Message arrived in topic ");
+//     DebugPrint(topic);
+//     DebugPrint(": ");
+//     // add string terminator
+//     char* message = (char*) payload;
+//     message[length] = '\0';
+//     DebugPrintln(message);
+// }
 
-void sense() {
-  logger.log(LogLevel::INFO, "Sensing...");
-  ledState = !ledState;
-  digitalWrite(LED, ledState);
-}
+// void sense() {
+//   logger.log(LogLevel::INFO, "Sensing...");
+//   ledState = !ledState;
+//   digitalWrite(LED, ledState);
+// }
 
-unsigned long lastRun = millis() ;
-inline void mainLoop() {
-  if (millis() - lastRun >= probingTime) {
-    sense();
-    lastRun = millis();
-  }
-}
+// unsigned long lastRun = millis() ;
+// inline void mainLoop() {
+//   if (millis() - lastRun >= probingTime) {
+//     sense();
+//     lastRun = millis();
+//   }
+// }
 
 void setup() {
-    Serial.begin(115200);
-    DebugPrintln("Booting");
-    pinMode(LED, OUTPUT);
-    digitalWrite(LED, LOW);
-    wifi::setup(clientName);
-    mqtt::setup(clientName, subscriptions, callback);
+    // Serial.begin(115200);
+    // DebugPrintln("Booting");
+    // pinMode(LED, OUTPUT);
+    // digitalWrite(LED, LOW);
+    // wifi::setup(clientName);
+    // mqtt::setup(clientName, subscriptions, callback);
 }
 
 void loop() {
-    wifi::loop();
-    mqtt::loop();
-    mainLoop();
+    // wifi::loop();
+    // mqtt::loop();
+    // mainLoop();
 }
